e857a1da5d44878eb7f98d55ac6f6ac465bea4c7
Anthony <ajoelcg@gmail.com>
2024-08-07 18:47:05 -0500

    "Init commit"

---
+++ src/typings.d.ts
@@ -0,0 +1,6 @@
+// src/waypoints.d.ts
+declare module '@srexi/purecounterjs';
+declare module 'isotope-layout';
+declare module 'imagesloaded';
+declare module 'glightbox';
+declare module 'swiper';
